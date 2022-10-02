e698a5790b34cdbaaef509493696746bebce72e7
Mari525 <mmuuzzaa2011@ya.ru>
2022-10-02 12:43:39 +0300

    fix: round to 3 decimal places

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,15 @@ let mass = document.getElementById("mass");
 convertBtn.addEventListener("click", function() {
   let value = convertInput.value;
   if (!isNaN(value)) {
-    length.textContent = `${value} meters = ${value * 3.281} feet | ${value} feet = ${value * 0.305} meters`;
-    volume.textContent = `${value} liters = ${value * 0.264} gallons | ${value} gallons = ${value * 3.785} liters`;
-    mass.textContent = `${value} kilos = ${value * 2.204} pounds | ${value} pounds = ${value * 0.454} kilos`;
+    warning.textContent = "";
+    length.textContent = `${value} meters = ${(value * 3.281).toFixed(3)} feet | ${value} feet = ${(value * 0.305).toFixed(3)} meters`;
+    volume.textContent = `${value} liters = ${(value * 0.264).toFixed(3)} gallons | ${value} gallons = ${(value * 3.785).toFixed(3)} liters`;
+    mass.textContent = `${value} kilos = ${(value * 2.204).toFixed(3)} pounds | ${value} pounds = ${(value * 0.454).toFixed(3)} kilos`;
   }
   else {
     warning.textContent = "Please enter a number";
+    length.textContent = "";
+    volume.textContent = "";
+    mass.textContent = "";
   }
 })
