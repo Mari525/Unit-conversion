e3d8e3d21a679103f5fc262711ad81fa294bc308
Mari525 <mmuuzzaa2011@ya.ru>
2022-10-03 09:40:17 +0300

    fix: add check for empty value

--- src/js/index.js
+++ src/js/index.js
@@ -14,17 +14,22 @@ if (localStorage.getItem("number")) {
 convertBtn.addEventListener("click", function() {
   let value = convertInput.value;
   localStorage.setItem("number", value);
-  if (!isNaN(value)) {
-    warning.textContent = "";
-    length.textContent = `${value} meters = ${(value * 3.281).toFixed(3)} feet | ${value} feet = ${(value * 0.305).toFixed(3)} meters`;
-    volume.textContent = `${value} liters = ${(value * 0.264).toFixed(3)} gallons | ${value} gallons = ${(value * 3.785).toFixed(3)} liters`;
-    mass.textContent = `${value} kilos = ${(value * 2.204).toFixed(3)} pounds | ${value} pounds = ${(value * 0.454).toFixed(3)} kilos`;
+  if (value) {
+    if (!isNaN(value)) {
+      warning.textContent = "";
+      length.textContent = `${value} meters = ${(value * 3.281).toFixed(3)} feet | ${value} feet = ${(value * 0.305).toFixed(3)} meters`;
+      volume.textContent = `${value} liters = ${(value * 0.264).toFixed(3)} gallons | ${value} gallons = ${(value * 3.785).toFixed(3)} liters`;
+      mass.textContent = `${value} kilos = ${(value * 2.204).toFixed(3)} pounds | ${value} pounds = ${(value * 0.454).toFixed(3)} kilos`;
+    }
+    else {
+      warning.textContent = "Please enter a number";
+      length.textContent = "";
+      volume.textContent = "";
+      mass.textContent = "";
+    }
   }
   else {
     warning.textContent = "Please enter a number";
-    length.textContent = "";
-    volume.textContent = "";
-    mass.textContent = "";
   }
 })
 
